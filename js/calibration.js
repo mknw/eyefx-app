cf36b2ad2b7cd7a15ea5a17b3f980bd152ad8c14
mknw <michaelaccetto@gmail.com>
2022-01-10 13:17:01 +0100

    Calibration working, but working on recalibration

--- js/calibration.js
+++ js/calibration.js
@@ -1,12 +1,15 @@
+var nCalClicks = 2;
 var PointCalibrate = 0;
 var CalibrationPoints={};
+// var canvas = document.getElementById("plotting_canvas");
+
 
 /**
  * Clear the canvas and the calibration button.
  */
 function ClearCanvas(){
-  $(".Calibration").hide();
   var canvas = document.getElementById("plotting_canvas");
+  $(".Calibration").hide();
   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 }
 function DeleteCanvas(){
@@ -43,25 +46,30 @@ function helpModalShow() {
 * This function listens for button clicks on the html page
 * checks that all buttons have been clicked 5 times each, and then goes on to measuring the precision
 */
-$(document).ready(function(){
+$(document).ready(OperateCalibration);
+
+function OperateCalibration(){
   ClearCanvas();
   helpModalShow();
-     $(".Calibration").click(function(){ // click event on the calibration buttons
-
+  CalibrationPoints = {}
+  //console.log(CalibrationPoints)
+  $(".Calibration").click(function(){ // click event on the calibration buttons
+      //console.log('click!')
       var id = $(this).attr('id');
-
       if (!CalibrationPoints[id]){ // initialises if not done
+        //console.log("initializing cal point counter for", id)
         CalibrationPoints[id]=0;
       }
-      CalibrationPoints[id]++; // increments values
+      CalibrationPoints[id]++;
+		 //console.log(id, 'incremented: ', CalibrationPoints[id])
 
-      if (CalibrationPoints[id]==5){ //only turn to yellow after 5 clicks
+      if (CalibrationPoints[id]==nCalClicks){ //only turn to yellow after 5 clicks
         $(this).css('background-color','yellow');
         $(this).prop('disabled', true); //disables the button
         PointCalibrate++;
-      }else if (CalibrationPoints[id]<5){
+      }else if (CalibrationPoints[id]<nCalClicks){
         //Gradually increase the opacity of calibration points when click to give some indication to user.
-        var opacity = 0.2*CalibrationPoints[id]+0.2;
+        var opacity = 0.1*CalibrationPoints[id]+0.1;
         $(this).css('opacity',opacity);
       }
 
@@ -74,11 +82,9 @@ $(document).ready(function(){
             //using jquery to grab every element in Calibration class and hide them except the middle point.
             $(".Calibration").hide();
             $("#Pt5").show();
-
             // clears the canvas
             var canvas = document.getElementById("plotting_canvas");
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-
             // notification for the measurement process
             swal({
               title: "Calculating measurement",
@@ -90,17 +96,16 @@ $(document).ready(function(){
 
                 // makes the variables true for 5 seconds & plots the points
                 $(document).ready(function(){
-
-                  store_points_variable(); // start storing the prediction points
-
+                  store_points_variable();
                   sleep(5000).then(() => {
-                      stop_storing_points_variable(); // stop storing the prediction points
+                      stop_storing_points_variable(); 
                       var past50 = webgazer.getStoredPoints(); // retrieve the stored points
+                      webgazer.clearGazeListener() // stop recording predictions.
                       var precision_measurement = calculatePrecision(past50);
                       var accuracyLabel = "<a>Accuracy | "+precision_measurement+"%</a>";
-                      document.getElementById("Accuracy").innerHTML = accuracyLabel; // Show the accuracy in the nav bar.
+                      document.getElementById("Accuracy").innerHTML = accuracyLabel;
                       swal({
-                        title: "Your accuracy measure is " + precision_measurement + "%",
+                        title: "Achieved accuracy: " + precision_measurement + "%",
                         allowOutsideClick: false,
                         buttons: {
                           cancel: "Recalibrate",
@@ -109,14 +114,14 @@ $(document).ready(function(){
                       }).then(isConfirm => {
                           if (isConfirm){
                             //clear the calibration & hide the last middle button
-                            ClearCanvas();
+                            //ClearCanvas();
                             DeleteCanvas();
                             ShowWebsite()
 
                           } else {
                             //use restart function to restart the calibration
                             document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
-                            webgazer.clearData();
+                            //webgazer.clearData();
                             ClearCalibration();
                             ClearCanvas();
                             ShowCalibrationPoint();
@@ -127,7 +132,7 @@ $(document).ready(function(){
             });
           }
     });
-});
+};
 
 /**
  * Show the Calibration Points
@@ -142,7 +147,7 @@ function ShowCalibrationPoint() {
 */
 function ClearCalibration(){
   // Clear data from WebGazer
-
+  webgazer.clearData()
   $(".Calibration").css('background-color','red');
   $(".Calibration").css('opacity',0.2);
   $(".Calibration").prop('disabled',false);
@@ -150,6 +155,8 @@ function ClearCalibration(){
 
   CalibrationPoints = {};
   PointCalibrate = 0;
+  // reset worker output Interaction to be sent to Requirer.
+  outputInteraction = [] 
 }
 
 // sleep function because java doesn't have one, sourced from http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
--- js/main.js
+++ js/main.js
@@ -2,21 +2,9 @@ window.onload = async function() {
 
     webgazer.params.showVideoPreview = true;
     //start the webgazer tracker
-    await webgazer.setRegression('ridge') /* currently must set regression and tracker */
-        //.setTracker('clmtrackr')
-        .setGazeListener(function(data, clock) {
-          //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-          //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
-        })
-        .saveDataAcrossSessions(true)
-        .begin();
-        webgazer.showVideoPreview(true) /* shows all video previews */
-            .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
-            .applyKalmanFilter(true); /* Kalman Filter defaults to on. Can be toggled by user. */
-
+    //CreateCalibrationTracker()
     //Set up the webgazer video feedback.
     var setup = function() {
-
         //Set up the main canvas. The main canvas is used to calibrate the webgazer.
         var canvas = document.getElementById("plotting_canvas");
         canvas.width = window.innerWidth;
@@ -24,29 +12,44 @@ window.onload = async function() {
         canvas.style.position = 'fixed';
     };
     setup();
-
 };
+
 
 // Set to true if you want to save the data even if you reload the page.
 window.saveDataAcrossSessions = true;
-
 window.onbeforeunload = function() {
     webgazer.end();
 }
 
+var outputInteraction = []
+
+async function CreateCalibrationTracker() {
+	await webgazer.setRegression('ridge') /* Ridge regression + Kalman Filter */
+        // .setGazeListener(function(data, clock) {
+          //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+          //   console.log(clock); 
+        //})
+        .saveDataAcrossSessions(true)
+        .begin();
+        webgazer.showVideoPreview(true)
+        .showPredictionPoints(true); /* change to false to remove red dot */
+}
+
 /**
- * Restart the calibration process by clearing the local storage and reseting the calibration point
+ * Restart the calibration process by clearing the local storage and resetting the calibration point
  */
 function RestartCalibration(){
     document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
     // document.getElementBy // should 
-    webgazer.clearData();
+    // webgazer.clearData();
     ClearCalibration();
+    CreateCalibrationTracker()
     PopUpInstruction();
     var canvas = document.getElementById("plotting_canvas");
     canvas.style.display = "";
     websiteDisplay = document.getElementById("websiteContainer");
     websiteDisplay.style.display = "none"
+    // OperateCalibration()
 }
 
 function HideAllWebgazerPreviews(){
@@ -59,9 +62,75 @@ function HideAllWebgazerPreviews(){
   * The webpage code.
   **/
 function ShowWebsite() {
+	webgazer.showVideoPreview(false) // slows down tracking by a lottt
 	websiteDisplay = document.getElementById("websiteContainer");
 	websiteDisplay.style.display = "block"
+	webgazer.setGazeListener(function(data, clock) {
+				// (X, Y) coordinates: data.x, data.y: 
+				outputInteraction.push({
+					ms: clock, // elapsed MS since webgazer.begin() was called
+					x: data.x, // More info avail in the `data` object. It's 
+					y: data.y  // possible to console.log to see what's in it.
+				});
+        })
+}
+
+
+function SkipTask() {
+	swal({
+		title: "Are you sure?",
+		text: "We cannot grant this campaign will be available in the future. If something changed in the setting, such as head position or light condition, you can opt to Recalibrate the eye-tracker.",
+		icon: "warning",
+		buttons: {
+			cancel: "Back to task", 
+			skip: true,
+			Recalibrate: true,
+		},
+		dangerMode: true,
+	}).then((willSkip) => {
+		switch (willSkip) {
+			case "skip":
+				swal("Terminating...");
+				userSubmissionInput = document.getElementById("user-submission");
+				eyetrackDataInput = document.getElementById("eyetracking-data");
+				userSubmissionInput.value = 'invalid';
+				eyetrackDataInput.value = 'invalid';
+				// SUBMIT PROGRAMMATICALLY
+				break
+
+			case "recalibrate":
+				RestartCalibration(); // FIX
+
+			default:
+				// do nothing
+				console.log('Skipping the skipping.')
+		}
+	});
 }
 
 
 
+function ShowInputPrompt() {
+	webgazer.end() // todo: check if conflicts with window.onbeforeunload (or removethe latter altogether)
+	// add input questions
+	
+	swal({
+		title: 'Answer the prompt:',
+		text: 'What are the name of the authors of the White Paper published by Effect AI?', 
+		content: 'input', 
+		button: {
+			text: "Submit",
+			closeModal: true,
+		}
+	}).then(answer => {
+			userSubmissionInput = document.getElementById("user-submission");
+			eyetrackDataInput = document.getElementById("eyetracking-data");
+			userSubmissionInput.value = answer
+			eyetrackDataInput.value = JSON.stringify(outputInteraction)
+			swal({
+				text: 'Thank you for contributing! Your EFX will be transferred to your account once your submission has been validated. To earn more EFX, start another batch!',
+				icon: "success",
+				button: "Terminate"
+			})
+	})
+}
